c536ba7540fd0410bd152532d60010219ab81bf2
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-03-29 20:55:26 +0100

    add flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
     description = "Python 3.12 development environment";
 
-    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     outputs = { self, nixpkgs }:
     let
