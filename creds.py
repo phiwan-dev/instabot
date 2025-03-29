e6d0be37c5a14dce4f20af7b20c934875985a019
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-03-29 20:50:55 +0100

    add credentials file

---
+++ creds.py
@@ -0,0 +1,19 @@
+# File for storing the credentials for later use
+# so it doesn't need to be hard coded in other code.
+
+
+APP_SECRET: str = ""
+SHORT_LIVED_TOKEN: str = ""
+LONG_LIVED_TOKEN: str = ""
+REFRESHED_LONG_TOKEN: str = ""
+
+
+USERNAME: str = ""
+# query for instagram page, no access token required.
+# fbid_v2 is the instagram account userid
+# url = f"https://www.instagram.com/web/search/topsearch/?query={USERNAME}"
+USER_ID: str = ""
+ID: str = ""
+
+
+
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+    description = "Python 3.12 development environment";
+
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; config.cudaSupport = true; config.allowUnfree = true; };
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+
+                python312
+                (with python312Packages; [
+                    requests
+                ])
+            ];
+        };
+    };
+}
+
