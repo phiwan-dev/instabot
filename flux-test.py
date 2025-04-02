77104729c522f65f12ae265c502cf4fc0cc15dbc
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-04-02 23:32:29 +0200

    improve flux loading for multiple prompts

--- flux-test.py
+++ flux-test.py
@@ -6,6 +6,7 @@ import creds
 class Flux:
     def __init__(self, save_path: str) -> None:
         self.save_path = save_path
+        self.pipe = None
 
     def save_model(self, name: str = "black-forest-labs/FLUX.1-schnell"):
         from huggingface_hub import login
@@ -14,17 +15,20 @@ class Flux:
         pipeline.save_pretrained(self.save_path)
 
     def get_image(self, prompt: str):
-            pipeline = FluxPipeline.from_pretrained(self.save_path, torch_dtype=torch.float16)
-            pipeline.enable_sequential_cpu_offload()
-            image = pipeline(
-                prompt,
-                guidance_scale=3.5,
-                num_inference_steps=4,
-                #generator=torch.Generator("cpu").manual_seed(0),
-            ).images[0]
-            image.save(f"./images/test.png")
+        if self.pipe is None:
+            self.pipe = FluxPipeline.from_pretrained(self.save_path, torch_dtype=torch.float16)
+            self.pipe.enable_sequential_cpu_offload()
+        image = self.pipe(
+            prompt,
+            guidance_scale=3.5,
+            num_inference_steps=4,
+            #generator=torch.Generator("cpu").manual_seed(0),
+        ).images[0]
+        image.save(f"./images/test.png")
 
 
 if __name__ == "__main__":
     flux = Flux("./flux1-schnell")
     flux.get_image(prompt="a living room with a fireplace")
+    flux.get_image(prompt="a bedroom with a fireplace")
+    flux.get_image(prompt="a kitchen with a fireplace")
