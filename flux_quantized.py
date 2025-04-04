ecb65ea0465f36f8007f6f3a662945db170c732c
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-04-04 14:45:12 +0200

    rename flux quantized

---
+++ flux_quantized.py
@@ -0,0 +1,53 @@
+import os
+import torch
+from diffusers import FluxPipeline
+from diffusers import FluxTransformer2DModel
+from diffusers import BitsAndBytesConfig as DiffusersBitsAndBytesConfig
+from transformers import BitsAndBytesConfig as TransformersBitsAndBytesConfig
+from transformers import T5EncoderModel
+
+class Flux:
+    def __init__(self) -> None:
+        quant_config = TransformersBitsAndBytesConfig(load_in_8bit=True,)
+        text_encoder_2_8bit = T5EncoderModel.from_pretrained(
+            "flux1-schnell",
+            subfolder="text_encoder_2",
+            quantization_config=quant_config,
+            torch_dtype=torch.float16,
+        )
+
+        quant_config = DiffusersBitsAndBytesConfig(load_in_8bit=True,)
+        transformer_8bit = FluxTransformer2DModel.from_pretrained(
+            "flux1-schnell",
+            subfolder="transformer",
+            quantization_config=quant_config,
+            torch_dtype=torch.float16,
+        )
+
+        pipe = FluxPipeline.from_pretrained(
+            "black-forest-labs/FLUX.1-dev",
+            transformer=transformer_8bit,
+            text_encoder_2=text_encoder_2_8bit,
+            torch_dtype=torch.float16,
+            #device_map="balanced",
+        )
+        pipe.enable_model_cpu_offload()
+        pipe.to("cuda")
+        self.pipe = pipe
+
+    def get_image(self, prompt: str, save_path: str = "images/test.png") -> None:
+        pipe_kwargs = {
+            "prompt": prompt,
+            "guidance_scale": 3.5,
+            "num_inference_steps": 10,
+            "height": 1024,
+            "width": 1024,
+        }
+        image = self.pipe(**pipe_kwargs).images[0]
+        if not os.path.exists(save_path):
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        image.save(save_path)
+
+
+
+
