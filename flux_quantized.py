f175f7984fb8538f999f951aa06ed7be267a060e
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-04-22 14:59:00 +0200

    improve memory clear on flux

--- flux_quantized.py
+++ flux_quantized.py
@@ -1,4 +1,5 @@
 import os
+import gc
 import torch
 from diffusers import FluxPipeline
 from diffusers import FluxTransformer2DModel
@@ -7,6 +8,7 @@ from transformers import BitsAndBytesConfig as TransformersBitsAndBytesConfig
 from transformers import T5EncoderModel
 
 class Flux:
+
     def __init__(self) -> None:
         quant_config = TransformersBitsAndBytesConfig(load_in_8bit=True,)
         text_encoder_2_8bit = T5EncoderModel.from_pretrained(
@@ -29,10 +31,10 @@ class Flux:
             transformer=transformer_8bit,
             text_encoder_2=text_encoder_2_8bit,
             torch_dtype=torch.float16,
-            #device_map="balanced",
         )
         pipe.enable_model_cpu_offload()
         self.pipe = pipe
+
 
     def generate_image(self, prompt: str, save_path: str = "images/test.png") -> None:
         pipe_kwargs = {
@@ -42,14 +44,22 @@ class Flux:
             "height": 1024,
             "width": 1024,
         }
-        image = self.pipe(**pipe_kwargs).images[0]
+        image = self.pipe(**pipe_kwargs).images[0] # type: ignore
         if not os.path.exists(save_path):
             os.makedirs(os.path.dirname(save_path), exist_ok=True)
         image.save(save_path)
 
 
     def clear_memory(self) -> None:
-        self.pipe = None
+        del self.pipe
+        gc.collect()
         torch.cuda.empty_cache()
 
 
+def test_gc() -> None:
+    from time import sleep
+    flux = Flux()
+    flux.generate_image("A beautiful landscape with mountains and a river.")
+    flux.clear_memory()
+    # check if memory usage is low
+    sleep(5)
