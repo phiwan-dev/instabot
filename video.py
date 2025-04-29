1973f2e6cbf2cf6a6e9139de7689d10889fa3e64
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-04-29 23:25:38 +0200

    video change zoom to global instead of per image

--- video.py
+++ video.py
@@ -19,24 +19,20 @@ image_framerate: float = image_count / total_lenght
 intro_length: int = 2
 image_framerate: float = 5
 (
-    ffmpeg
-    .input("images/209/*.png", pattern_type="glob", framerate=image_framerate, loop=1)
+    ffmpeg.input("images/209/*.png", pattern_type="glob", loop=1)
     # upscale to fix jitter from zoom
     .filter(
-        'scale',
+        "scale",
         w="5000",
-        h='-1',  # maintain aspect ratio
-        force_original_aspect_ratio='decrease'
+        h="-1",  # maintain aspect ratio
     )
     .filter(
-        'zoompan',
-        z=f'min(zoom+0.001, 1.9)',          # Zoom factor expression. Constant means zoom TO this level over 'd' frames.
-                                    # Can use expressions like 'min(zoom+0.001, 1.5)' for continuous zoom.
-        x='(iw-iw/zoom)/2',     # X position expression (centers horizontally)
-        y='(ih-ih/zoom)/2',     # Y position expression (centers vertically)
-        d=15,                # Duration of the zoom/pan effect FOR EACH input frame, in number of OUTPUT frames.
-        s="720x1280",          # Output frame size (WidthxHeight)
-        fps="30"              # Frame rate of the output FROM THIS FILTER.
+        "zoompan",
+        z=f"pow({1.05}, on/{image_count})",
+        d=output_fps * image_length,  # for how long each image should be displayed
+        x="(iw-iw/zoom)/2",
+        y="(ih-ih/zoom)/2",
+        s="720x1280",
     )
     .output("reel_intro.mp4", vcodec="libx264", r=30, pix_fmt="yuv420p", t=intro_length)
     .run(overwrite_output=True)
