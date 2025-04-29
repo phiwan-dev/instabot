6bdd522a90ebe5961ed8e4ec0f9baea9cb3aaebc
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-04-29 18:48:45 +0200

    add video intro zoom

--- video.py
+++ video.py
@@ -21,6 +21,16 @@ image_framerate: float = 5
 (
     ffmpeg
     .input("images/209/*.png", pattern_type="glob", framerate=image_framerate, loop=1)
+    .filter(
+        'zoompan',
+        z=f'min(zoom+0.001, 1.9)',          # Zoom factor expression. Constant means zoom TO this level over 'd' frames.
+                                    # Can use expressions like 'min(zoom+0.001, 1.5)' for continuous zoom.
+        x='(iw-iw/zoom)/2',     # X position expression (centers horizontally)
+        y='(ih-ih/zoom)/2',     # Y position expression (centers vertically)
+        d=15,                # Duration of the zoom/pan effect FOR EACH input frame, in number of OUTPUT frames.
+        s="720x1280",          # Output frame size (WidthxHeight)
+        fps="30"              # Frame rate of the output FROM THIS FILTER.
+    )
     .output("reel_intro.mp4", vcodec="libx264", r=30, pix_fmt="yuv420p", t=intro_length)
     .run(overwrite_output=True)
 )
