35f5facc5cc8f3f2eb2185b7d228974b52b821cb
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-05-01 18:41:38 +0200

    video add zoom to main part

--- video.py
+++ video.py
@@ -14,11 +14,26 @@ output_fps: float = 30
 main_lenght: int = 10  # Total length of the video
 image_count: int = 4  # Number of ai generated images to include in the video
 images_per_second: float = (image_count / main_lenght)  # 1/the time each image is displayed
+image_length: float = main_lenght / image_count
+zoom_level: float = 1.001
 
 # Define FFmpeg processing
 # main video
 (
-    ffmpeg.input(os.path.join(image_dir, "*.png"), pattern_type="glob", framerate=images_per_second)
+    ffmpeg.input(os.path.join(image_dir, "*.png"), pattern_type="glob")
+    # upscale to fix jitter from zoom
+    .filter(
+        "scale",
+        w="5000",
+        h="-1",  # maintain aspect ratio
+    )
+    # zoom per image, zoom variable resets every input image
+    .filter("zoompan",
+            z=f"zoom*{zoom_level}",
+            d=output_fps * image_length,    # d = duration of each image
+            x="iw/2-(iw/zoom/2)",
+            y="ih/2-(ih/zoom/2)",
+            s="720x1280")
     .output("reel_main.mp4", vcodec="libx264", r=output_fps, pix_fmt="yuv420p")  # H.264 codec
     .run(overwrite_output=True)
 )
@@ -26,7 +41,7 @@ images_per_second: float = (image_count / main_lenght)  # 1/the time each image 
 
 # fast intro
 intro_length: float = 2
-image_length: float = 0.2
+image_length = 0.2
 (
     ffmpeg.input(os.path.join(image_dir, "*.png"), pattern_type="glob", loop=1)
     # upscale to fix jitter from zoom
