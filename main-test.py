4ecfa51a9a20747098abe93107d43d26d2ca1338
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-05-22 01:12:04 +0200

    dev: outline main file

---
+++ main-test.py
@@ -0,0 +1,26 @@
+
+
+
+# word
+    # image prompts
+    # caption
+    # perhaps comment
+    # vlog script
+
+
+# flux
+    # images
+
+
+# audio
+    # audio
+
+
+# video
+    # reel_intro
+    # reel_main
+    # reel_total
+
+
+# api-instagram
+
